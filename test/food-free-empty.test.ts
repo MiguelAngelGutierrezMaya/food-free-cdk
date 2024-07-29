f4037fadf7aa48b6f5f5a495d348ac2637c8305c
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2024-07-29 15:34:17 -0500

    Initial commit

---
+++ test/food-free-empty.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FoodFreeEmpty from '../lib/food-free-empty-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/food-free-empty-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FoodFreeEmpty.FoodFreeEmptyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
